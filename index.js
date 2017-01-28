f3ae21921a8b550c3fd74b4d44ed575d3c5ba8c8
Felipe Pontes <felipemfpontes@gmail.com>
2017-01-28 11:02:26 -0300

    Add new tests

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-'use strict';
+'use strict'
 
-const axios = require('axios');
+const axios = require('axios')
 
 const api = axios.create({
-  baseURL: 'https://suap.ifrn.edu.br/api'
-});
+  baseURL: 'https://suap.ifrn.edu.br/api/'
+})
 
 const autenticar = (matricula, senha, callback) => {
   if (!matricula || !senha || !callback) {
-    return callback(new Error('Argumentos inválidos.'));
+    return callback(new Error('Argumentos inválidos.'))
   }
-  api.post('/autenticacao/token/', {
+  api.post('autenticacao/token/', {
       username: matricula,
       password: senha
     })
     .then((response) => {
-      api.defaults.headers.common['Authorization'] = `Token ${response.data.token}`;
+      api.defaults.headers.common['Authorization'] = `Token ${response.data.token}`
       return callback(null, response.data);
     })
     .catch((error) => {
       if (error.response) {
-        return callback(new Error('Falha na autenticação.'));
+        return callback(new Error('Falha na autenticação.'))
       }
-      return callback(new Error('Falha na tentativa de autenticação.'));
-    });
+      return callback(new Error('Falha na tentativa de autenticação.'))
+    })
 }
 
 module.exports = {
-  autenticar: autenticar
-};
+  autenticar
+}
--- test/index.js
+++ test/index.js
@@ -1,16 +1,61 @@
-import test from 'ava'
-import Suap from '../index.js'
+const test = require('ava')
+const Suap = require('../index.js')
+const nock = require('nock')
 
 test.cb('deve retornar erro quando matrícula for vazia', t => {
   Suap.autenticar('', 'minh4senh4', (err, data) => {
-    t.true(err instanceof Error);
-    t.end();
-  });
-});
+    t.true(err instanceof Error)
+    t.end()
+  })
+})
 
 test.cb('deve retornar erro quando senha for vazia', t => {
   Suap.autenticar('20161014040012', '', (err, data) => {
-    t.true(err instanceof Error);
-    t.end();
-  });
-});
+    t.true(err instanceof Error)
+    t.end()
+  })
+})
+
+test.cb('deve retornar token', t => {
+  nock('https://suap.ifrn.edu.br/api/')
+    .post('/autenticacao/token/', {
+      username: '20161014040012',
+      password: 'minh4senh4'
+    })
+    .reply(200, { token: 'abcdefghijklmnopqrstuvwxyz0123456789' })
+
+  Suap.autenticar('20161014040012', 'minh4senh4', (err, data) => {
+    t.deepEqual(data, {
+      token: 'abcdefghijklmnopqrstuvwxyz0123456789'
+    })
+    t.end()
+  })
+})
+
+test.cb('deve retornar erro de autenticacao', t => {
+  nock('https://suap.ifrn.edu.br/api/')
+    .post('/autenticacao/token/', {
+      username: '20161014040012',
+      password: 's3nh4errad4'
+    })
+    .reply(403)
+
+  Suap.autenticar('20161014040012', 's3nh4errad4', (err, data) => {
+    t.true(err instanceof Error)
+    t.end()
+  })
+})
+
+test.cb('deve retornar erro na solicitacao', t => {
+  nock('https://suap.ifrn.edu.br/api/')
+    .post('/autenticacao/token/', {
+      username: '20161014040012',
+      password: 'minh4senh4'
+    })
+    .replyWithError('No connection.')
+
+  Suap.autenticar('20161014040012', 'minh4senh4', (err, data) => {
+    t.true(err instanceof Error)
+    t.end()
+  })
+})
